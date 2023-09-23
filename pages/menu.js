445491fc2417a7e0a6fb1b164572e337215c82b6
Alex Sendre <rares.sendre@gmail.com>
2023-09-23 10:16:26 -0500

    altered mock data and some styling changes

--- pages/menu.js
+++ pages/menu.js
@@ -15,7 +15,7 @@ const menu = (array) => {
           <h4 class="menu-card-title">${res.itemName}</h4>
           <hr style="width:30%;margin:0 auto;margin-top:10px;">
           <h6 class="card-body">${res.itemDescription}</h6>
-          <h5 class="card-title">PRICE: ${res.itemPrice}</h5>
+          <h5 class="card-title">$${res.itemPrice}</h5>
           <div class="menu-btns" style="position:absolute;margin: 0 auto;left:0;right:0;bottom:13px;">
             <i class="btn btn-dark" style="color:white;width:40%;border: 2px solid darkred" id="menu-item-edit-btn--${res.firebaseKey}">Edit Item</i>
             <i class="btn btn-danger" style="width:40%;border: 2px solid black;"id="menu-item-delete-btn--${res.firebaseKey}">Delete Item</i>
