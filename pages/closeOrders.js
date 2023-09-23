eb146c4e9445ecf1cfc75857ca1dccb5a601802a
Joey Ebach <connect@joeyebach.com>
2023-09-23 09:17:26 -0500

    added tip amount buttons and revenue data in close order details page

--- pages/closeOrders.js
+++ pages/closeOrders.js
@@ -6,18 +6,25 @@ const closeOrders = (orderId, total) => {
 
   let domString = '';
   domString += `
-  <form id='close-order-form--${orderId}--${total}'>
-    <select class="form-select" id='close-order-payment-type' aria-label="Payment Type">
+  <form id='close-order-form--${orderId}--${total}' style="width:332.02px">
+    <p>Payment Type</p>
+    <select class="form-select" id='close-order-payment-type'  aria-label="Payment Type">
       <option selected>Select A Payment Type</option>
       <option value="cash">Cash</option>
       <option value="credit">Credit</option>
       <option value="mobile">Mobile</option>
     </select>
-    <div class="mb-3">
+    <div class="mb-3" id="tip-amount">
       <label for="close-order-tip-input" class="form-label">Tip Amount</label>
-      <input type="number" name="currency" class="form-control" id="close-order-tip-input" aria-describedby="emailHelp">
+      <input type="number" min="1" step="any" class="form-control" id="close-order-tip-input" aria-describedby="emailHelp">
     </div>
-    <button type="submit" class="btn btn-primary" id="close-order-form--${orderId}--${total}">Close Order</button>
+    <div id="tip-auto">
+      <p onclick="document.querySelector('#close-order-tip-input').value = (${total} * .1).toFixed(2)">10%</p>
+      <p onclick="document.querySelector('#close-order-tip-input').value = (${total} * .15).toFixed(2)">15%</p>
+      <p onclick="document.querySelector('#close-order-tip-input').value = (${total} * .2).toFixed(2)">20%</p>
+      <p onclick="document.querySelector('#close-order-tip-input').value = (${total} * 1).toFixed(2)">100%</p>
+    </div>
+    <button type="submit" class="btn btn-dark" id="close-order-form--${orderId}--${total}">Close Order</button>
   </form>
   `;
 
